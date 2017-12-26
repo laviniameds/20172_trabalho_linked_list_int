f3af6d5928a49175c921a71c76a22074131c8b7c
laviniameds <laviniameds@icloud.com>
2017-12-25 23:53:09 -0300

    ->teste desempenho funcoes;

--- test_01.c
+++ test_01.c
@@ -4,7 +4,7 @@
 int main(){
   linked_list_int l1 = lli_create();
   int i,t;
-  for (i=0; i < 8 ; ++i)
+  for (i=0; i < 1000 ; ++i)
     lli_push_back(l1,i*3+1);
   t = lli_size(l1);
   printf("Tamanho de l1: %d\n",t);
@@ -12,20 +12,30 @@ int main(){
   for (i=0; i < t ; ++i)
     printf("%d ", lli_get(l1,i));
   printf("}\n");
-  printf("%d\n", lli_find(l1, 1));
 
-  /*for (i=0; i < 8 ; ++i)
-    lli_pop_back(l1);*/
 
-  lli_insert_at(l1, 2, 5);
-  t = lli_size(l1);
+  printf("O numero %d está no indice %d\n", 22, lli_find(l1, 22));
+
+
+  lli_insert_at(l1, 5, 21);
   printf("Tamanho de l1: %d\n",t);
   printf("l1 = { ");
-  for (i=0; i < t ; ++i)
+  for (i=0; i < lli_size(l1); ++i)
     printf("%d ", lli_get(l1,i));
   printf("}\n");
+
+
 
   lli_remove_from(l1, 5);
+  printf("Tamanho de l1: %d\n",t);
+  printf("l1 = { ");
+  for (i=0; i < lli_size(l1) ; ++i)
+    printf("%d ", lli_get(l1,i));
+  printf("}\n");
+
+
+  for (i=0; i < 1000 ; ++i)
+    lli_pop_back(l1);
   t = lli_size(l1);
   printf("Tamanho de l1: %d\n",t);
   printf("l1 = { ");
@@ -33,6 +43,6 @@ int main(){
     printf("%d ", lli_get(l1,i));
   printf("}\n");
 
-  lli_destroy(l1);
+  /*lli_destroy(l1);*/
   return 0;
 }
